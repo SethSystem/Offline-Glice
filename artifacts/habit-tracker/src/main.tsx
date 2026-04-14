284c6d7a05c664cd2206915f79bad9082c65261c
SethSystem <168155908+SethSystem@users.noreply.github.com>
2026-04-14 18:07:07 -0300

    Refactor main.tsx to use ReactDOM for rendering

--- artifacts/habit-tracker/src/main.tsx
+++ artifacts/habit-tracker/src/main.tsx
@@ -1,8 +1,10 @@
-<QueryClientProvider client={queryClient}>
-  <TooltipProvider>
-    <WouterRouter>
-      <Router />
-    </WouterRouter>
-    <Toaster />
-  </TooltipProvider>
-</QueryClientProvider>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
